1ef94bf0770326264f28e1e52153028edf000f18
wry <wry10150@163.com>
2021-07-18 17:25:41 +0800

    [ADD] 为Bean对象注入属性和依赖Bean的功能实现

--- springframework/src/main/java/com/wry/springframework/beans/factory/config/BeanDefinition.java
+++ springframework/src/main/java/com/wry/springframework/beans/factory/config/BeanDefinition.java
@@ -1,10 +1,26 @@
 package com.wry.springframework.beans.factory.config;
 
+import com.wry.springframework.beans.factory.support.PropertyValues;
+
 public class BeanDefinition {
     private Class beanClass;
+    private PropertyValues propertyValues;
 
     public BeanDefinition(Class beanClass) {
         this.beanClass = beanClass;
+    }
+
+    public BeanDefinition(Class beanClass, PropertyValues propertyValues) {
+        this.beanClass = beanClass;
+        this.propertyValues = propertyValues;
+    }
+
+    public PropertyValues getPropertyValues() {
+        return propertyValues;
+    }
+
+    public void setPropertyValues(PropertyValues propertyValues) {
+        this.propertyValues = propertyValues;
     }
 
     public Class getBeanClass() {
--- springframework/src/main/java/com/wry/springframework/beans/factory/support/AbstractAutowireCapableBeanFactory.java
+++ springframework/src/main/java/com/wry/springframework/beans/factory/support/AbstractAutowireCapableBeanFactory.java
@@ -1,9 +1,12 @@
 package com.wry.springframework.beans.factory.support;
 
+import cn.hutool.core.bean.BeanUtil;
 import com.wry.springframework.beans.BeansException;
 import com.wry.springframework.beans.factory.config.BeanDefinition;
+import com.wry.springframework.beans.factory.config.BeanReference;
 
 import java.lang.reflect.Constructor;
+import java.util.Objects;
 
 public abstract class AbstractAutowireCapableBeanFactory extends AbstractBeanFactory {
 
@@ -13,7 +16,10 @@ public abstract class AbstractAutowireCapableBeanFactory extends AbstractBeanFac
     protected Object createBean(String beanName, BeanDefinition beanDefinition, Object... args) throws BeansException {
         Object bean = null;
         try {
-            bean = this.createBeanInstance(beanDefinition, beanName, args);
+            //创建bean对象
+            bean = this.createBeanInstance(beanName, beanDefinition, args);
+            // 给 Bean 填充属性
+            this.applyPropertyValues(beanName, bean, beanDefinition);
         } catch (BeansException e) {
             throw new BeansException("Instantiation of bean failed", e);
         }
@@ -22,7 +28,30 @@ public abstract class AbstractAutowireCapableBeanFactory extends AbstractBeanFac
         return bean;
     }
 
-    protected Object createBeanInstance(BeanDefinition beanDefinition, String beanName, Object[] args) {
+    protected void applyPropertyValues(String beanName, Object bean, BeanDefinition beanDefinition) {
+        try {
+            PropertyValues propertyValues = beanDefinition.getPropertyValues();
+            if (Objects.nonNull(propertyValues)) {
+                for (PropertyValue propertyValue : propertyValues.getPropertyValues()) {
+                    String name = propertyValue.getName();
+                    Object value = propertyValue.getValue();
+
+                    if (value instanceof BeanReference) {
+                        // A 依赖 B，获取 B 的实例化,递归调用
+                        BeanReference beanReference = (BeanReference) value;
+                        value = super.getBean(beanReference.getBeanName());
+                    }
+                    // 属性填充
+                    BeanUtil.setFieldValue(bean, name, value);
+                }
+            }
+        } catch (Exception e) {
+            throw new BeansException("Error setting property values：" + beanName);
+        }
+    }
+
+
+    protected Object createBeanInstance(String beanName, BeanDefinition beanDefinition, Object[] args) {
         Constructor constructorToUse = null;
         Class<?> beanClass = beanDefinition.getBeanClass();
         Constructor<?>[] declaredConstructors = beanClass.getDeclaredConstructors();
